980d63679a59167ef0a5609e0b1be6c425b344a2
danishbutt <danishbutt_db@hotmail.com>
2021-10-10 13:12:20 +0500

    Flip Cards on Click 3

--- src/UserCard.js
+++ src/UserCard.js
@@ -1,10 +1,7 @@
 import React from 'react'
-
 import ReactCardFlip from 'react-card-flip'
 import './style/cardStyle.css'
-
 import { useState } from 'react'
-
 
 
 const UserCard = ({ userData }) => {
@@ -24,9 +21,7 @@ const UserCard = ({ userData }) => {
 
     }
 
-
-    const on_Click = () => {
-
+    let changeFlipSide = () => {
         FlipStyle == "vertical" ? setFlipStyle("horizontal") : setFlipStyle("vertical");
         isFlipped == true ? setFlipped(false) : setFlipped(true);
 
@@ -40,25 +35,22 @@ const UserCard = ({ userData }) => {
 
 
             <div className="mainCard">
-                <ReactCardFlip isFlipped={isFlipped} flipDirection={FlipStyle}  >
+                <ReactCardFlip isFlipped={isFlipped} flipDirection={FlipStyle} >
 
-                    <div className="card" onMouseOver={onHover} onClick={on_Click}  >
+                    <div className="card" onMouseOver={onHover} onClick={changeFlipSide}  >
                         <img className="card_image" src={userData.picture.large} />
-
                         <h1 className="card_title">  {userData.name.first}  </h1>
-
                         <h1 className="card_age"> <i class="fa-solid fa-user"></i> {userData.name.first} {userData.name.last}  </h1>
                         <h3 className="card_cell"> <i class="fa-solid fa-phone"></i> {userData.cell}  </h3>
 
                     </div>
 
-                    <div className="card" onMouseOut={flipBack} onClick={on_Click} >
+                    <div className="card" onMouseOut={flipBack} onClick={changeFlipSide} >
 
                         <i class="fa-brands fa-audible fa-3x"></i>
-
                         <h3 className="card_email">  {userData.email}  </h3>
                         <h3 className="card_location">
-                            <i class="fa-solid fa-location-dot"></i> {userData.location.street.name}  </h3>
+                        <i class="fa-solid fa-location-dot"></i> {userData.location.street.name}  </h3>
 
 
 
